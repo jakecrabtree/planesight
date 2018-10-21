9a1d6f2fbe7f64743dd7abad3004a1a95f981efe
imcdo <ian_mcdonald@rocketmail.com>
2018-10-21 04:22:33 -0500

    fruit

---
+++ planesight/Assets/src/Scripts/BuddyObj/FruitBundle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitBundle : MonoBehaviour {
+
+    float timeSinceBite;
+    [SerializeField]float EatSpeed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Buddy") {
+            timeSinceBite += Time.deltaTime;
+            if (timeSinceBite >= EatSpeed) TakeBite();
+        }       
+
+    }
+
+    public void TakeBite()
+    {
+        timeSinceBite = 0;
+        Transform child = transform.GetChild(0);
+        if (child == null) Destroy(gameObject);
+        else Destroy(child.gameObject);
+    }
+}
