d7e3ab600c6b81a4c3e2a4a570cd569cf03093e4
imcdo <ian_mcdonald@rocketmail.com>
2018-10-21 06:13:35 -0500

    hat

---
+++ planesight/Assets/src/Scripts/ItemManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour {
+
+    [SerializeField] List<string> keys;
+    [SerializeField] List<GameObject> values;
+
+    Dictionary<string, GameObject> mapping;
+
+	// Use this for initialization
+	void Start () {
+		for (int i=0; i < keys.Count; i++)
+        {
+            mapping.Add(keys[i], values[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void toggleActive(string key)
+    {
+        GameObject obj = mapping[key];
+        obj.SetActive(!obj.active);
+    }
+}
