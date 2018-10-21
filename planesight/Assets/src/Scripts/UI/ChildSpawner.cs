143d587ded136d6d4cdd580eab619cc920d4733d
imcdo <ian_mcdonald@rocketmail.com>
2018-10-21 02:01:14 -0500

    derp

---
+++ planesight/Assets/src/Scripts/UI/ChildSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChildSpawner : MonoBehaviour {
+
+    [SerializeField] private List<GameObject> childrenToSpawn;
+    [SerializeField] Canvas mCanvas;
+    [SerializeField] TrayItem ti;
+    float TotalW;
+    private RectTransform rt;
+    private List<RectTransform> childrenRts;
+    private float initW;
+    private float initH;
+
+    // Use this for initialization
+    void Start () {
+        rt = GetComponent<RectTransform>();
+        childrenRts = new List<RectTransform>();
+ 
+		foreach(GameObject child in childrenToSpawn)
+        {
+            TrayItem curr = Instantiate(ti, transform);
+            curr.obj = child;
+            initW = curr.GetComponent<RectTransform>().rect.width;
+            initH = curr.GetComponent<RectTransform>().rect.height;
+            TotalW += initW;
+            childrenRts.Add(curr.GetComponent<RectTransform>());
+        }
+        Debug.Log("" + TotalW + "  " + rt.rect.width);
+	}
+
+    void Update()
+    {
+        if (TotalW > rt.rect.width)
+        {
+            Debug.Log("here");
+            float newW = rt.rect.width / childrenRts.Count;
+            foreach (RectTransform crt in childrenRts)
+            {
+                crt.sizeDelta = new Vector2(newW, initH);
+            }
+        }
+        else
+        {
+            foreach (RectTransform crt in childrenRts)
+            {
+                crt.sizeDelta = new Vector2(initW, initH);
+            }
+        }
+
+
+    }
+}
---
+++ planesight/Assets/src/Scripts/UI/Tray.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tray : MonoBehaviour {
+    Rect mRect;
+    Transform trayTransform;
+
+    Vector3 origin;
+    Vector3 end;
+
+    float speed;
+
+
+	// Use this for initialization
+	void Start () {
+        trayTransform = transform.parent;
+        mRect = trayTransform.gameObject.GetComponent<RectTransform>().rect;
+
+        origin = transform.position;
+        end = new Vector3(
+            Screen.width - GetComponent<RectTransform>().rect.width / 2,
+            origin.y,
+            origin.z
+            );
+        speed = 0;        
+	}
+	
+	void OnMouseDrag()
+    {
+    }
+ 
+   
+}
